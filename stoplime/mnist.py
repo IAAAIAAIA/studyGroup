27937df1c04bcbc8a70a0931a18ac9ace0edfa34
stoplime <slanimero@gmail.com>
2017-03-09 23:41:27 -0500

    best mnist of 0.019188

--- stoplime/mnist.py
+++ stoplime/mnist.py
@@ -4,8 +4,11 @@ plt.rcParams['figure.figsize'] = (7,7) # Make the figures a bit bigger
 
 from keras.datasets import mnist
 from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation
+from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.utils import np_utils
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
 
 nb_classes = 10
 
@@ -14,7 +17,62 @@ nb_classes = 10
 print("X_train original shape", X_train.shape)
 print("y_train original shape", y_train.shape)
 
-for i in range(9):
-    plt.subplot(3,3,i+1)
-    plt.imshow(X_train[i], cmap='gray', interpolation='none')
-    plt.title("Class {}".format(y_train[i]))
+X_train = X_train.reshape(60000, 28, 28, 1)
+X_test = X_test.reshape(10000, 28, 28, 1)
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+print("Training matrix shape", X_train.shape)
+print("Testing matrix shape", X_test.shape)
+
+Y_train = np_utils.to_categorical(y_train, nb_classes)
+Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+
+model = Sequential()
+model.add(Convolution2D(64, 3, 3, border_mode='same', input_shape=(28, 28, 1)))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(3, 3)))
+model.add(Dropout(0.2))
+
+model.add(Convolution2D(128, 3, 3, border_mode='same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(3, 3)))
+model.add(Dropout(0.2))
+
+model.add(Flatten())
+
+model.add(Dense(512))
+model.add(BatchNormalization())
+model.add(Activation('tanh'))
+model.add(Dropout(0.45))
+
+model.add(Dense(10))
+model.add(BatchNormalization())
+model.add(Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+model.fit(X_train, Y_train,
+          batch_size=128, nb_epoch=30,
+          verbose=1,
+          validation_data=(X_test, Y_test))
+
+score = model.evaluate(X_test, Y_test, verbose=0)
+print('Test score:', score)
+
+# The predict_classes function outputs the highest probability class
+# according to the trained classifier for each input example.
+predicted_classes = model.predict_classes(X_test)
+
+# Check which items we got right / wrong
+correct_indices = np.nonzero(predicted_classes == y_test)[0]
+incorrect_indices = np.nonzero(predicted_classes != y_test)[0]
+
+print(correct_indices.shape[0]/10000)
+print("accuracy: " + str(correct_indices.shape[0]/10000))
+
+model.save("save2.h5")
