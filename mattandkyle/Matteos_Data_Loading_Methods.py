ee9b8429e62a60886fb7656349a7f18d7c369aea
stoplime <slanimero@gmail.com>
2017-06-19 18:40:18 -0400

    more of the same

--- mattandkyle/Matteos_Data_Loading_Methods.py
+++ mattandkyle/Matteos_Data_Loading_Methods.py
@@ -1,18 +1,23 @@
 
 # coding: utf-8
 
-# In[5]:
+# In[8]:
 
 import keras
 import csv
 import os
+import cv2
 import progressbar as pb
+import numpy as np
+import PIL
+import pandas as pd
 
-# In[1]:
+
+# In[10]:
 
 def load_X_train_data(n_images):
     # Chose path to the folder containing the training data in .jpg format:
-    train_data_path = '/home/sexy/CS231n/mat/Kaggle/train-jpg'
+    train_data_path = '/home/sexy/Documents/KaggleData/train-tif-v2'
     # Chose number of images to load.
     # Type 'all' to load all the images in the folder, or 'half' to load half of them
 
@@ -25,7 +30,7 @@ def load_X_train_data(n_images):
 
 def load_X_test_data(n_images):
     # Chose path to the folder containing the test data in .jpg format:
-    test_data_path = '/home/sexy/CS231n/mat/Kaggle/test-jpg'
+    test_data_path = '/home/sexy/Documents/KaggleData/train-tif-v2'
     # Chose number of images to load.
     # Type 'all' to load all the images in the folder, or 'half' to load half of them
     print('Loading Test Data: ')
@@ -37,7 +42,7 @@ def load_X_test_data(n_images):
 
 def load_Y_data(n_images):
     # Chose path to the .csv file containing the labels: 
-    csv_path = '/home/sexy/CS231n/mat/Kaggle/train.csv'
+    csv_path = '/home/sexy/Documents/KaggleData/train_v2.csv'
 
     image_and_tags = csv_reader(csv_path)[:n_images]
     labels = label_lister(image_and_tags)
@@ -64,7 +69,7 @@ def load_jpg_images(folder, N):
             break
         # print("\n", _filename[1], "testing:)")
         filename = _filename[1]
-        img = np.array(PIL.Image.open(os.path.join(folder, filename)))/255
+        img = np.array(cv2.imread(os.path.join(folder, filename)))/255
         if img is not None:
             images.append(img)
             filenames.append(filename)
@@ -102,9 +107,125 @@ def list_to_vec(list_img_labels, all_labels):
             if all_labels[j] in list_img_labels[i]:
                 vec[i][j] = 1
             else:
-                vec[i][j] = 0
+                vec[i][j] = -1
     pbar.finish()
     return vec
+
+class Data_Generators:
+    
+    def __init__(self, X_path, y_path, train_decimal=.7, val_decimal=.2, test_decimal=.1, batch_size=500):
+        #X_path should be path to folder containing image files
+        #y_path should be a csv file of the labels
+        #_decimal inputs should be the decimal portion of data that is of each set
+        #all should add up to 1
+        if round(train_decimal + val_decimal + test_decimal) != 1.0:
+            raise ValueError('train_decimal, val_decimal, and test_decimal arguements should add up to 1')
+        self.X_path = X_path
+        self.y_path = y_path
+        self.batch_size = batch_size
+        total = len(os.listdir(self.X_path))
+        self.num_train = (int)(total * train_decimal)
+        self.num_val = (int)(total * val_decimal)
+        self.num_test = total - (self.num_train + self.num_val)
+        
+    def csv_labels_to_numbers(self, array, labels, category):
+        #used to convert array of all label int values to array of single binary value for each picture
+        array = np.array(array)
+        new_array = []
+        if category in labels:
+            cat_index = labels.index(category)
+        else:
+            raise ValueError('Incorrect category value')
+        for i in range(array.shape[0]):
+            new_array.append(array[i,cat_index])
+        return new_array
+    
+    def X_train(self):
+        file_list = os.listdir(self.X_path)
+        _list_n = [(int(''.join(list(filter(str.isdigit, x)))), file_list[i]) for i, x in enumerate(file_list)]
+        # print(_list_n[0])
+        _list_n = sorted(_list_n, key=getkey)
+        #pbar = pb.ProgressBar(widgets=[pb.Percentage(), pb.Bar(), ], max_value=N).start() # max_value=len(list)).start()
+        iterations = (int)(self.num_train / self.batch_size if self.num_train % self.batch_size is 0
+                           else self.num_train / self.batch_size + 1)
+        for i in range(iterations - 1):
+            images = []
+            if i*self.batch_size+self.batch_size < self.num_train:
+                for _filename in _list_n[i*self.batch_size: i*self.batch_size+self.batch_size]:                                                                                                      
+                    filename = _filename[1]
+                    img = np.array(cv2.imread(os.path.join(self.X_path, filename)))/255
+                    if img is not None:
+                        images.append(img)
+            else:
+                for _filename in _list_n[i*self.batch_size: num_train]:                                                                                        
+                    filename = _filename[1]
+                    img = np.array(PIL.Image.open(os.path.join(self.X_path, filename)))/255
+                    if img is not None:
+                        images.append(img)
+            yield images
+
+    def y_train(self, category):
+        iterations = (int)(self.num_train / self.batch_size if self.num_train % self.batch_size is 0 
+                           else self.num_train / self.batch_size + 1)
+        for i in range(iterations - 1):
+            if self.batch_size*i+self.batch_size >= self.num_train:
+                image_and_tags = csv_reader(self.y_path)[self.batch_size*i: num_train]
+            else:
+                image_and_tags = csv_reader(self.y_path)[self.batch_size*i: self.batch_size*i+self.batch_size]
+            
+            labels = label_lister(image_and_tags)
+            Y_train_all = list_to_vec(image_and_tags['tags'], labels)
+            Y_train = self.csv_labels_to_numbers(Y_train_all, labels, category)
+            yield Y_train
+            
+    def X_val(self):
+        file_list = os.listdir(self.X_path)
+        _list_n = [(int(''.join(list(filter(str.isdigit, x)))), file_list[i]) for i, x in enumerate(file_list)]
+        _list_n = sorted(_list_n, key=getkey)
+        images = []
+        for _filename in _list_n[self.num_train: self.num_train+self.num_val]:
+            filename = _filename[1]
+            img = np.array(cv2.imread(os.path.join(self.X_path, filename)))/255
+            if img is not None:
+                 images.append(img)
+        images = np.array(images)
+        return images
+            
+    def y_val(self, category):
+        image_and_tags = csv_reader(self.y_path)[self.num_train: self.num_train+self.num_val]
+        labels = label_lister(image_and_tags)
+        Y_train_all = list_to_vec(image_and_tags['tags'], labels)
+        Y_train = self.csv_labels_to_numbers(Y_train_all, labels, category)
+        Y_train = np.array(Y_train)
+        return Y_train
+            
+
+    def X_test(self):
+        file_list = os.listdir(self.X_path)
+        _list_n = [(int(''.join(list(filter(str.isdigit, x)))), file_list[i]) for i, x in enumerate(file_list)]
+        _list_n = sorted(_list_n, key=getkey)
+        images = []
+        for _filename in _list_n[self.num_train+self.num_val:]:
+            filename = _filename[1]
+            img = np.array(cv2.imread(os.path.join(self.X_path, filename)))/255
+            if img is not None:
+                 images.append(img)
+        images = np.array(images)
+        return images
+            
+    def y_test(self, category):
+        image_and_tags = csv_reader(self.y_path)[self.num_train+self.num_val:]
+        labels = label_lister(image_and_tags)
+        Y_train_all = list_to_vec(image_and_tags['tags'], labels)
+        Y_train = self.csv_labels_to_numbers(Y_train_all, labels, category)
+        images = np.array(images)
+        return Y_train
+            
+   
+
+# In[ ]:
+
+
 
 
 # In[ ]:
