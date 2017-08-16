d10f40ee83781d46275162fb5d0c927ebae7b152
stoplime <slanimero@gmail.com>
2017-08-16 09:52:52 -0400

    testing

---
+++ steffen/_test.py
@@ -0,0 +1,10 @@
+import nltk
+# nltk.download()
+from nltk.corpus import wordnet as wn
+
+word = wn.synset('chicken.n.01')
+hyper = lambda s: s.hypernyms()
+print (word)
+word_hyper = list(word.closure(hyper))
+for i in enumerate(word_hyper):
+    print(i)
