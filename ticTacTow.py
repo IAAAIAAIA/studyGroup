e40a1a43f3febb5848df77988119bc0f95cb1c0a
stoplime <slanimero@gmail.com>
2017-12-17 14:57:30 -0500

    test TicTacTow

--- ticTacTow.py
+++ ticTacTow.py
@@ -1,15 +1,15 @@
 # the tic tac tow game
 import numpy as np
+from scipy import signal
 
-class board():
+class board(object):
     def __init__(self):
         self.connectLength = 3
         self.boardWidth = 3
         self.boardHeight = 3
         self.players = 2
-        self.player1Piece = "X"
-        self.player2Piece = "O"
-        self.boards = np.array((self.boardWidth, self.boardHeight, self.players))
+        self.playerPieces = ["X", "O"]
+        self.boards = np.zeros((self.boardWidth, self.boardHeight, self.players))
         self.defineWinFilters()
     
     def Play(self, player, location):
@@ -17,31 +17,37 @@ class board():
             return False
         
         if type(player) == str:
-            if player == self.player1Piece:
-                playerType = 1
-            elif player == self.player2Piece:
-                playerType = 2
+            # handles the case for string implementations
+            for i, piece in enumerate(self.playerPieces):
+                if player == piece:
+                    playerType = i
         else:
+            # handles the case for int pieces implementation
             playerType = player
-        
-        if all(self.boards[location, :] == 0):
-            self.boards[location, int(playerType)] = 1
+        # print(self.boards)
+        if all(self.boards[location[0], location[1], :] == 0):
+            self.boards[location[0], location[1], int(playerType)] = 1
             return True
         return False
     
     def CheckWin(self):
         # convolve about the board with win filters
-
+        for i, piece in enumerate(self.playerPieces):
+            for j, convFilter in enumerate(self.filters):
+                conv = signal.convolve2d(self.boards[:,:,i], convFilter, 'valid')
+                if np.any(conv >= self.connectLength):
+                    return piece
+        return False
         
     def defineWinFilters(self):
         if self.connectLength <= self.boardWidth and self.connectLength <= self.boardHeight:
             self.filters = []
             # row filter
-            self.filters.append(np.ones(self.connectLength, 1))
+            self.filters.append(np.ones((self.connectLength, 1)))
             # col filter
-            self.filters.append(np.ones(1, self.connectLength))
+            self.filters.append(np.ones((1, self.connectLength)))
             # top-left diagonal
-            box = np.zeros(self.connectLength, self.connectLength)
+            box = np.zeros((self.connectLength, self.connectLength))
             for i in range(self.connectLength):
                 box[i, i] = 1
             self.filters.append(box)
@@ -52,4 +58,35 @@ class board():
             self.filters.append(box)
         else:
             raise ValueError('ConnectLength is larger than board size.')
+    
+    def Print(self):
+        for i in range(self.boardWidth):
+            for j in range(self.boardHeight):
+                if any(self.boards[i,j,:] != 0):
+                    for k in range(self.players):
+                        if self.boards[i,j,k] != 0:
+                            print(self.playerPieces[k], end='')
+                else:
+                    print(" ", end='')
+                if j != self.boardHeight-1:
+                    print(" | ", end='')
+                else:
+                    print()
+            if i != self.boardWidth-1:
+                print("---------")
 
+def main():
+    game = board()
+    game.Play("X", (0,0))
+    game.Play("X", (1,1))
+    game.Play("X", (1,2))
+    game.Play("O", (1,0))
+    game.Play("O", (2,0))
+    game.Play("O", (2,2))
+    game.Play("O", (2,1))
+    result = game.CheckWin()
+    print("winner:", result)
+    game.Print()
+
+if __name__ == '__main__':
+    main()
